dced4961aba19093ad589ff2e066b9057e297418
Domas <114532410+Domukas22@users.noreply.github.com>
2023-11-17 21:44:28 +0100

    Mobile styling improvements

--- src/logic/1_Nav/Nav.jsx
+++ src/logic/1_Nav/Nav.jsx
@@ -18,51 +18,69 @@ export function Nav({ currFOLDER, availFOLDERS, SET_dispFolderID, TOGGLE_form, s
 
   return (
     <nav>
-      <div className="navBTN chooseFolder" onClick={() => TOGGLE_folders()} data-open={AREfoldersOpen}>
-        {currFOLDER.title}
-        <div className="navDROPDOWN">
-          {availFOLDERS.map((folder) => {
-            return (
-              <div
-                className="navDropdownITEM"
-                key={folder.id}
-                data-current={folder.id === currFOLDER.id}
-                onClick={() => SET_dispFolderID(folder.id)}
-              >
-                {folder.title}
-              </div>
-            );
-          })}
+      <div className="contentWRAP">
+        <div className="navBTN chooseFolder" onClick={() => TOGGLE_folders()} data-open={AREfoldersOpen}>
+          {currFOLDER.title}
+          <div className="navDROPDOWN">
+            {availFOLDERS.map((folder) => {
+              return (
+                <div
+                  className="navDropdownITEM"
+                  key={folder.id}
+                  data-current={folder.id === currFOLDER.id}
+                  onClick={() => SET_dispFolderID(folder.id)}
+                >
+                  {folder.title}
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <input
+          className="inputDesktop"
+          type="search"
+          name=""
+          id=""
+          placeholder="Search..."
+          onChange={(e) => {
+            SET_searchText(e.target.value.toLowerCase());
+          }}
+        />
+        <div className="navBTN sort" onClick={() => TOGGLE_filters()} data-open={ISsortOpen}>
+          {sorting}
+          <div className="navDROPDOWN">
+            <div
+              className="navDropdownITEM"
+              data-current={sorting === "Shuffle"}
+              onClick={() => SET_sorting("Shuffle")}
+            >
+              Shuffle
+            </div>
+            <div className="navDropdownITEM" data-current={sorting === "Color"} onClick={() => SET_sorting("Color")}>
+              Color
+            </div>
+            <div className="navDropdownITEM" data-current={sorting === "Date"} onClick={() => SET_sorting("Date")}>
+              Date
+            </div>
+          </div>
+        </div>
+        <div className="navBTN addTR" onClick={() => TOGGLE_form(true)}>
+          <div className="xWRAP">
+            <div className="xLINE"></div>
+            <div className="xLINE second"></div>
+          </div>
         </div>
       </div>
-      <input
-        type="search"
-        name=""
-        id=""
-        placeholder="Search..."
-        onChange={(e) => {
-          SET_searchText(e.target.value.toLowerCase());
-        }}
-      />
-      <div className="navBTN sort" onClick={() => TOGGLE_filters()} data-open={ISsortOpen}>
-        {sorting}
-        <div className="navDROPDOWN">
-          <div className="navDropdownITEM" data-current={sorting === "Shuffle"} onClick={() => SET_sorting("Shuffle")}>
-            Shuffle
-          </div>
-          <div className="navDropdownITEM" data-current={sorting === "Color"} onClick={() => SET_sorting("Color")}>
-            Color
-          </div>
-          <div className="navDropdownITEM" data-current={sorting === "Date"} onClick={() => SET_sorting("Date")}>
-            Date
-          </div>
-        </div>
-      </div>
-      <div className="navBTN addTR" onClick={() => TOGGLE_form(true)}>
-        <div className="xWRAP">
-          <div className="xLINE"></div>
-          <div className="xLINE second"></div>
-        </div>
+      <div className="contentWRAP mobile">
+        <input
+          type="search"
+          name=""
+          id=""
+          placeholder="Search..."
+          onChange={(e) => {
+            SET_searchText(e.target.value.toLowerCase());
+          }}
+        />
       </div>
     </nav>
   );
